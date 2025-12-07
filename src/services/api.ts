a4a8ac15ccbb89ec795c79fbd17147f70bb9a7ba
Reepls Project <reepls.project@gmail.com>
2025-12-07 07:17:30 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://reepls-app-vrosp.ondigitalocean.app/';
 
 export const apiClient = axios.create({
   baseURL: `${BASE_URL}/api-v1`,
